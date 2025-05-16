6857f4552f05dea4645e76ee48a959c88d222792
syedrazahussain <149230428+syedrazahussain@users.noreply.github.com>
2025-05-16 20:47:23 +0530

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-    return (
 
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            fetch('/api/heartbeat')
+                .then(response => {
+                    if (!response.ok) {
+                        console.error('Heartbeat ping failed');
+                    }
+                })
+                .catch(err => {
+                    console.error('Error sending heartbeat ping:', err);
+                });
+        }, 300000); // every 5 minutes
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
         <div>
             <Header />
-
 
             <div className="main_content">
                 <div className="four_div">
@@ -29,20 +45,15 @@ const Home = () => {
                             <p>Past Events & Contacts</p>
                         </div>
                     </Link>
-                    
+
                     <Link to="/profiledashboard" className='Link_div'>
                         <div className="box">
                             <p>Manage Profile</p>
                         </div>
                     </Link>
-
-
-
                 </div>
             </div>
-
         </div>
-
     )
 }
 
