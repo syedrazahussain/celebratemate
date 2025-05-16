19135775b54990054f861b862f66cef8e1de48ea
syedrazahussain <149230428+syedrazahussain@users.noreply.github.com>
2025-05-16 09:56:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,10 @@ app.use((req, res, next) => {
 
 
 const pool = new Pool({
-  connectionString: process.env.PG_URL
+  connectionString: process.env.PG_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 
