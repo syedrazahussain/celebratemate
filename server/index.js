a893fe188450bfae219912b8d26e83b5e668979a
SYED RAZA HUSSAIN <149230428+syedrazahussain@users.noreply.github.com>
2025-11-17 23:13:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,12 +14,7 @@ const { sendSimpleEmail } = require('./sendEmail'); // new SendGrid module
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 app.use(cors({
   origin: [
